9c9b6c650185fdd5288e789254403c395833e1a2
nyugoh <nyugoh@gmail.com>
2018-10-12 05:45:36 +0300

    Added list to display orders

--- app/src/main/java/campusorders/com/quebasetech/joe/campusorders/buyer_home.java
+++ app/src/main/java/campusorders/com/quebasetech/joe/campusorders/buyer_home.java
@@ -1,16 +1,55 @@
 package campusorders.com.quebasetech.joe.campusorders;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class buyer_home extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    private Context context;
+    private ListView itemsList;
+    private String[] items = {
+            "Smokies",
+            "Smokies & eggs",
+            "Eggs",
+            "Eggs",
+            "Apples",
+            "Apples",
+            "Groceries",
+            "Milk",
+            "Milk",
+    };
+    private Double[] prices = {
+            22.0,
+            40.0,
+            40.0,
+            20.0,
+            25.0,
+            25.0,
+            75.0,
+            35.0,
+            35.0,
+    };
+    private String[] sellers = {
+            "Jane Doe",
+            "Awesome Dude",
+            "Awesome Dude",
+            "John Doe",
+            "Joe Nyugoh",
+            "Joe Nyugoh",
+            "Fisher Maxwell",
+            "Milly Bett",
+            "Milly Bett",
+    };
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -39,6 +78,18 @@ public class buyer_home extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        context = this;
+        itemsList = (ListView) findViewById(R.id.items_list_view);
+        Items_Adapter adapter = new Items_Adapter(context, items, prices, sellers);
+        itemsList.setAdapter(adapter);
+
+        itemsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // TODO:: Launch dialog for each
+                Toast.makeText(context, "You selected: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
