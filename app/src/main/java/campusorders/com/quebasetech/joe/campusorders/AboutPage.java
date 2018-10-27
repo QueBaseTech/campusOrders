20ad709c9a3fad8594d9b85df360a0cf76050241
nyugoh <nyugoh@gmail.com>
2018-10-27 18:33:16 +0300

    Added about app information

--- app/src/main/java/campusorders/com/quebasetech/joe/campusorders/AboutPage.java
+++ app/src/main/java/campusorders/com/quebasetech/joe/campusorders/AboutPage.java
@@ -24,5 +24,6 @@ public class AboutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+        getSupportActionBar().hide();
     }
 }
