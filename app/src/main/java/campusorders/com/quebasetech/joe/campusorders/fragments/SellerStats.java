2670113f0d899df68095f37679828b64f3289f74
nyugoh <nyugoh@gmail.com>
2018-10-30 01:07:01 +0300

    :muscle: Added minor changes, button resize and arrangement, added stats templete

--- app/src/main/java/campusorders/com/quebasetech/joe/campusorders/fragments/SellerStats.java
+++ app/src/main/java/campusorders/com/quebasetech/joe/campusorders/fragments/SellerStats.java
@@ -15,5 +15,6 @@ public class SellerStats extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.seller_stats, container, false);
+
     }
 }
